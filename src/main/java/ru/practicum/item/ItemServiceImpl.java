e428f7aa706a2c4cb09eba7cf51a889a07bd2f21
Oksana Chufistova <oksana.chufistova@gmail.com>
2025-06-18 21:03:05 +0200

    Implement transaction settings

--- src/main/java/ru/practicum/item/ItemServiceImpl.java
+++ src/main/java/ru/practicum/item/ItemServiceImpl.java
@@ -1,14 +1,14 @@
 package ru.practicum.item;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 public class ItemServiceImpl implements ItemService {
     private final ItemRepository itemRepository;
 
@@ -21,11 +21,13 @@ public class ItemServiceImpl implements ItemService {
     }
 
     @Override
+    @Transactional
     public void deleteItem(long userId, long itemId) {
         itemRepository.deleteByUserIdAndId(userId, itemId);
     }
 
     @Override
+    @Transactional
     public ItemDto addNewItem(long userId, ItemDto itemDto) {
         Item item = itemRepository.save(ItemMapper.mapToItem(itemDto, userId));
         return ItemMapper.mapToItemDto(item);
